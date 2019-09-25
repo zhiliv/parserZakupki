d2c94e16616b5ab49531b65be17e56450adde735
zhiliv <35070562+zhiliv@users.noreply.github.com>
2019-09-25 09:21:44 +0300

    исправление ошибок

--- app/modules/funcParse.js
+++ app/modules/funcParse.js
@@ -7,16 +7,18 @@
 
 //подключение пакетов
 const Nick = require('nickjs'), //парсер
-	q = require('q'), //пакет для работы с promises
-	jsdom = require('jsdom'),
-	dom = new jsdom.JSDOM(),
-	window = dom.window,
-	document = window.document,
-	$ = require('jquery')(window),
-	async = require('async');
+  q = require('q'), //пакет для работы с promises
+  jsdom = require('jsdom'),
+  dom = new jsdom.JSDOM(),
+  window = dom.window,
+  $ = require('jquery')(window),
+  async = require('async'),
+  parser = require('./parser')
 
 //создание экземаляра парсера с таймаутом 10000мс
-var nick = new Nick({ timeout: 15000 });
+var nick = new Nick({
+  timeout: 15000
+});
 /**
  * получение кода страницы по ссылке
  * @function getData
@@ -25,30 +27,33 @@ var nick = new Nick({ timeout: 15000 });
  * @memberof funcParse
  */
 var getDataBody = async (tab, ind) => {
-	//отспрочка выполнения
-	let defer = q.defer();
-	//объект для храения результата
-	let result = { data: null, err: null };
-	//поытка открытия страницы
-	if (ind == 1) {
-		await tab.waitUntilPresent('[ng-if="withState"]', 15000);
-	}
-	//параметры парсинга(получение содердимого Body)
-	const scraper = (arg, done) => {
-		//получение содерердимого тега BODy
-		done(null, $('body').html());
-	};
-	if (!result.err) {
-		//попытка содержимое страницы
-		try {
-			const html = await tab.evaluate(scraper);
-			result.data = html;
-		} catch (err) {
-			result.err = err;
-		}
-	}
-	defer.resolve(result);
-	return defer.promise;
+  //отспрочка выполнения
+  let defer = q.defer();
+  //объект для храения результата
+  let result = {
+    data: null,
+    err: null
+  };
+  //поытка открытия страницы
+  if (ind == 1) {
+    await tab.waitUntilPresent('[ng-if="withState"]', 15000);
+  }
+  //параметры парсинга(получение содердимого Body)
+  const scraper = (arg, done) => {
+    //получение содерердимого тега BODy
+    done(null, $('body').html());
+  };
+  if (!result.err) {
+    //попытка содержимое страницы
+    try {
+      const html = await tab.evaluate(scraper);
+      result.data = html;
+    } catch (err) {
+      result.err = err;
+    }
+  }
+  defer.resolve(result);
+  return defer.promise;
 };
 
 /**
@@ -59,9 +64,9 @@ var getDataBody = async (tab, ind) => {
  * @memberof funcParse
  */
 var newTap = async () => {
-	//создание вкладки
-	const tab = await nick.newTab();
-	return tab;
+  //создание вкладки
+  const tab = await nick.newTab();
+  return tab;
 };
 
 /**
@@ -73,19 +78,22 @@ var newTap = async () => {
  * @memberof funcParse
  */
 var openTab = async (tab, url) => {
-	//отспрочка выполнения
-	let defer = q.defer();
-	//объект для храения результата
-	let result = { data: null, err: null };
-	//поытка открытия страницы
-	try {
-		await tab.open(url);
-		result.data = tab;
-	} catch (err) {
-		result.err = err;
-	}
-	defer.resolve(result);
-	return defer.promise;
+  //отспрочка выполнения
+  let defer = q.defer();
+  //объект для храения результата
+  let result = {
+    data: null,
+    err: null
+  };
+  //поытка открытия страницы
+  try {
+    await tab.open(url);
+    result.data = tab;
+  } catch (err) {
+    result.err = err;
+  }
+  defer.resolve(result);
+  return defer.promise;
 };
 
 /**
@@ -96,16 +104,16 @@ var openTab = async (tab, url) => {
  * @memberof funcParse
  */
 var getLengthListPage = html => {
-	//перевод кода в jquery
-	let body = $.parseHTML(html);
-	//получение содержимого BODY
-	let text = $(body)
-		.find('[ng-if="withState"]')
-		.text();
-	//удаление надписи
-	let lengthPages = text.replace('Найдено: ', '');
-	//возврат значения в числовом типе
-	return Number(lengthPages);
+  //перевод кода в jquery
+  let body = $.parseHTML(html);
+  //получение содержимого BODY
+  let text = $(body)
+    .find('[ng-if="withState"]')
+    .text();
+  //удаление надписи
+  let lengthPages = text.replace('Найдено: ', '');
+  //возврат значения в числовом типе
+  return Number(lengthPages);
 };
 
 /**
@@ -113,60 +121,60 @@ var getLengthListPage = html => {
  * @function getListProduct
  * @param {String} html Код страницы
  * @
-  */
+ */
 var getListProduct = html => {
-	let defer = q.defer();
-	let body = $.parseHTML(html);
-	let list = $(body).find('#listView div [class="col-12 description"]');
-	async.eachOfSeries(list, async (row, ind) => {
-		let obj = {};
-		obj.name = $(row)
-			.find('.o_name')
-			.text();
+  let defer = q.defer();
+  let body = $.parseHTML(html);
+  let list = $(body).find('#listView div [class="col-12 description"]');
+  async.eachOfSeries(list, async (row, ind) => {
+    let obj = {};
+    obj.name = $(row)
+      .find('.o_name')
+      .text();
     //console.log("TCL: obj.name", obj.name)
-		let href = $(row)
-			.find('.o_name')
-			.attr('href');
+    let href = $(row)
+      .find('.o_name')
+      .attr('href');
     obj.id = get_id(href);
-    let param = ''
     let listParam = $(row).find('[ng-repeat="characteristic in dataItem.skuCharacteristics"]');
-    await getListParam(listParam)
-    let i = $(document).find('span.k-state-selected').text()
-	});
-	return list.length;
+    await getListParam(listParam).then(res => {
+      obj.param = res
+      //console.log("TCL: obj", obj)
+    })
+    parser.arr.push(obj)
+  });
+  return list.length;
 };
 
 var getListParam = async list => {
   let defer = q.defer();
   let result = ''
-  if(list.length)
-  await async.eachOfSeries(list, async(row, ind) => {
-    let text = $(row).text()
-    let element = text.split(':');
-    if(ind == 0){
-      result = `{"${format_element(element[0])}": "${format_element(element[1])}",`
-    }
-    else{
-      if(ind != list.length-1){
-        result += `"${format_element(element[0])}": "${format_element(element[1])}",`
+  if (list.length)
+    await async.eachOfSeries(list, async (row, ind) => {
+      let text = $(row).text()
+      let element = text.split(':');
+      if (ind == 0) {
+        result = `{"${format_element(element[0])}": "${format_element(element[1])}",`
+      } else {
+        if (ind != list.length - 1) {
+          result += `"${format_element(element[0])}": "${format_element(element[1])}",`
+        } else {
+          result += `"${format_element(element[0])}": "${format_element(element[1])}"}`
+        }
       }
-      else{
-        result += `"${format_element(element[0])}": "${format_element(element[1])}"}`
+      if (ind == list.length - 1) {
+        //console.log("TCL: result", result)
+        result = JSON.parse(result)
+        defer.resolve(result)
       }
-    }
-    if(ind == list.length-1){
-      result = JSON.parse(result)
-      //console.log("TCL: result", result)
-      defer.resolve(result)
-    }
-  })
-  return defer
+    })
+  return defer.promise;
 }
 
 var format_element = element => {
   element = element.trim()
-  element = element.replace('\\', '/');
-  element = element.replace(`"`, `'`)
+  element = element.replace(/\\/g,"||")
+  element = element.replace(/(['"])/g, "\\$1")
   return element
 }
 
@@ -178,22 +186,28 @@ var format_element = element => {
  * @memberof funcParse
  */
 var get_id = href => {
-	//удаление символов
-	href = href.replace('https://', '');
-	//разбиение ссылки на массив
-	let list = href.split('/');
-	//получениуе полденего элемента массмива
-	let id = list[list.length - 1];
-	return id;
+  //удаление символов
+  href = href.replace('https://', '');
+  //разбиение ссылки на массив
+  let list = href.split('/');
+  //получениуе полденего элемента массмива
+  let id = list[list.length - 1];
+  return id;
 };
 
-var nextPage = async tab => {
-  try {
-    await tab.waitUntilPresent('span .k-state-selected', 15000)
-    await tab.click(selector)
-  } catch (err) {
-    console.log("An error occured:", err)
-  }
+
+var saveToFile = () => {
+  var fs = require('fs');
+var filename = 'output.txt';
+var str = JSON.stringify(parser.arr, null, 4);
+
+fs.writeFile(filename, str, function(err){
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('File written!');
+    }
+});
 }
 
 module.exports.getDataBody = getDataBody;
@@ -205,3 +219,5 @@ module.exports.newTab = newTap;
 module.exports.openTab = openTab;
 
 module.exports.getListProduct = getListProduct;
+
+module.exports.saveToFile = saveToFile;
--- app/modules/parser.js
+++ app/modules/parser.js
@@ -9,7 +9,8 @@ let countOnPage = 50;
 //подулючение модля с ффункциями для парсинга
 const fn = require('./funcParse');
 
-let ind = 1;
+let arr = []
+let ind = 0;
 let lengthPages = 1;
 /**
  * Функция запуска парсинга
@@ -18,54 +19,55 @@ let lengthPages = 1;
  */
 module.exports.start = async () => {
   let e;
-	let arrErr = [];
-	//стартовая ссылка
-	let url = `https://old.zakupki.mos.ru/#/sku?s.costPerUnitLessEqual=5000&s.productionDirectoryPaths.0=.1.72095941.&s.state=actual&s.entityStateIn.0=1&v.ps=${countOnPage}&v.s=relevance&v.sd`;
+  let arrErr = [];
+  //стартовая ссылка
+  let url = `https://old.zakupki.mos.ru/#/sku?s.costPerUnitLessEqual=5000&s.productionDirectoryPaths.0=.1.72095941.&s.state=actual&s.entityStateIn.0=1&v.ps=${countOnPage}&v.s=relevance&v.sd`;
 
-	//переменная для хранения вкладки
-	var tab;
-	//создание новой вкладки
-	await fn.newTab().then(res => {
-		tab = res;
-	});
-	//открытие страницы во вкладке
-	await fn.openTab(tab, url).then(res => {
-		res.err ? (err = res.err) : (tab = res.data);
-	});
+  //переменная для хранения вкладки
+  var tab;
+  //создание новой вкладки
+  await fn.newTab().then(res => {
+    tab = res;
+  });
+  //открытие страницы во вкладке
+  await fn.openTab(tab, url).then(res => {
+    res.err ? (err = res.err) : (tab = res.data);
+  });
 
-	//цикл для обхода всех страниц
-	while (true) {
-		//переменная для зранения текста ошибки
-		let err = null;
-
-		let html;
-		if (!err) {
-			//получение BODY у страницы
-			await fn.getDataBody(tab, ind).then(res => {
-				//проверка на наличие ошибок
-				res.err ? (err = res.err) : (html = res.data);
-			});
-		}
-		if (!err) {
-			if (ind == 1) {
-				//получение найденных значений
-				lengthPages = await fn.getLengthListPage(html);
-				lengthPages = Math.ceil(lengthPages / countOnPage);
+  //цикл для обхода всех страниц
+  while (true) {
+    await tab.wait(2000)
+    //переменная для зранения текста ошибки
+    let err = null;
+    let html;
+    if (!err) {
+      //получение BODY у страницы
+      await fn.getDataBody(tab, ind).then(res => {
+        //проверка на наличие ошибок
+        res.err ? (err = res.err) : (html = res.data);
+      });
+    }
+    if (!err) {
+      if (ind == 0) {
+        //получение найденных значений
+        lengthPages = await fn.getLengthListPage(html);
+        lengthPages = Math.ceil(lengthPages / countOnPage);
+        console.log("TCL: module.exports.start -> lengthPages", lengthPages)
       }
       try {
-        await tab.waitUntilPresent(`a.k-link:contains(${ind+1})`, 15000)
-        await tab.click(`a.k-link:contains("${ind+1}")`)
+        await tab.click(`.k-i-arrow-e`)
       } catch (err) {
         console.log("An error occured:", err)
       }
       fn.getListProduct(html)
       ind++
-      if(ind == lengthPages){
+      if (ind == lengthPages+1) {
+        console.log("Длина", arr.length)
+        fn.saveToFile()
         break
       }
-      tab
-			//fn.getListProduct(html)
-			console.log('TCL: module.exports.start -> fn.getListProduct(html)', fn.getListProduct(html));
-		}
-	}
+      //fn.getListProduct(html)
+    }
+  }
 };
+module.exports.arr = arr;
