3357ef543e1e42b16f2dafc22d22b9d35ca6e093
zhiliv <35070562+zhiliv@users.noreply.github.com>
2019-09-25 10:25:42 +0300

    исправление ошибок

--- app/modules/funcParse.js
+++ app/modules/funcParse.js
@@ -13,14 +13,15 @@ const Nick = require('nickjs'), //парсер
   window = dom.window,
   $ = require('jquery')(window),
   async = require('async'),
-  parser = require('./parser')
+    parser = require('./parser')
 
 //создание экземаляра парсера с таймаутом 10000мс
 var nick = new Nick({
-  timeout: 15000
+  timeout: 20000
 });
 /**
  * получение кода страницы по ссылке
+ * @async
  * @function getData
  * @param {String} url Ссылка для получения данных со страницы
  * @return { Object} data - данные; err: ошибка
@@ -71,6 +72,7 @@ var newTap = async () => {
 
 /**
  * Открытие страницы на вкладке
+ * @async
  * @function openTab
  * @param {Object} tab Вкладка
  * @param {String} url Ссылка
@@ -122,48 +124,72 @@ var getLengthListPage = html => {
  * @param {String} html Код страницы
  * @
  */
-var getListProduct = html => {
-  let defer = q.defer();
+var getListProduct = async  html => {
+  //получение тела HTML
   let body = $.parseHTML(html);
+  //получение спика
   let list = $(body).find('#listView div [class="col-12 description"]');
-  async.eachOfSeries(list, async (row, ind) => {
+  //обход в цикле всего списка
+  await async.eachOfSeries(list, async (row, ind) => {
+    //объявление объекта дял хранения характеристик проудукта
     let obj = {};
+    //получение имени
     obj.name = $(row)
       .find('.o_name')
       .text();
-    //console.log("TCL: obj.name", obj.name)
+    //получение ссылки на продукт 
     let href = $(row)
       .find('.o_name')
       .attr('href');
+    //получене идентификатор из ссылки
     obj.id = get_id(href);
+    //получение блока с параметрами
     let listParam = $(row).find('[ng-repeat="characteristic in dataItem.skuCharacteristics"]');
+    //получение параметров
     await getListParam(listParam).then(res => {
+      //добалвение параметров всвойство объекта
       obj.param = res
       //console.log("TCL: obj", obj)
     })
     parser.arr.push(obj)
   });
-  return list.length;
+
 };
 
+/** 
+ * Получение параметров из блока
+ * @async
+ * @function getListParam
+ * @param {Array} list Список элементов
+ * @result {Object} JSON с парастрами
+ * @memberof funcParse
+ */
 var getListParam = async list => {
   let defer = q.defer();
+  //создание пустой переменной для текста
   let result = ''
+  //проверка длины списка элементов
   if (list.length)
+    //обход всех элементов в цикле
     await async.eachOfSeries(list, async (row, ind) => {
+      //получение текста элемента
       let text = $(row).text()
+      //разбиение элемента на массив
       let element = text.split(':');
       if (ind == 0) {
+        //первая строка JSOn
         result = `{"${format_element(element[0])}": "${format_element(element[1])}",`
       } else {
         if (ind != list.length - 1) {
           result += `"${format_element(element[0])}": "${format_element(element[1])}",`
         } else {
+          //полсденяя строка JSON
           result += `"${format_element(element[0])}": "${format_element(element[1])}"}`
         }
       }
       if (ind == list.length - 1) {
         //console.log("TCL: result", result)
+        //преобразование текста в JSON
         result = JSON.parse(result)
         defer.resolve(result)
       }
@@ -171,9 +197,18 @@ var getListParam = async list => {
   return defer.promise;
 }
 
+/**
+ * Форматирование элемента
+ * @functuon format_element
+ * @param {String} element Содержимое элемента
+ * @memberof
+ */
 var format_element = element => {
+  //удаление пробелов в начале и конце строки
   element = element.trim()
-  element = element.replace(/\\/g,"||")
+  //замена слешей
+  element = element.replace(/\\/g, "||")
+  //замена двойных кавычек
   element = element.replace(/(['"])/g, "\\$1")
   return element
 }
@@ -196,18 +231,28 @@ var get_id = href => {
 };
 
 
+/**
+ * Сохранение в файл
+ * @function saveToFile
+ * @memberof funcParse
+ */
 var saveToFile = () => {
+  //модуль для работы с файловой системой
   var fs = require('fs');
-var filename = 'output.txt';
-var str = JSON.stringify(parser.arr, null, 4);
+  //имя получаемого файла
+  var filename = 'отчет.txt';
+  //преобразование массива в текст
+  var str = JSON.stringify(parser.arr, null, 4);
 
-fs.writeFile(filename, str, function(err){
-    if(err) {
-        console.log(err)
+  //запись файла
+  fs.writeFile(filename, str, function (err) {
+    if (err) {
+      console.log(err)
     } else {
-        console.log('File written!');
+      console.log(`Обработано ${parser.arr.length}`)
+      console.log('Файл создан успешно');
     }
-});
+  });
 }
 
 module.exports.getDataBody = getDataBody;
--- app/modules/parser.js
+++ app/modules/parser.js
@@ -12,13 +12,13 @@ const fn = require('./funcParse');
 let arr = []
 let ind = 0;
 let lengthPages = 1;
+let lengPage = 1
 /**
  * Функция запуска парсинга
  * @memberof parser
  * @async
  */
 module.exports.start = async () => {
-  let e;
   let arrErr = [];
   //стартовая ссылка
   let url = `https://old.zakupki.mos.ru/#/sku?s.costPerUnitLessEqual=5000&s.productionDirectoryPaths.0=.1.72095941.&s.state=actual&s.entityStateIn.0=1&v.ps=${countOnPage}&v.s=relevance&v.sd`;
@@ -31,14 +31,15 @@ module.exports.start = async () => {
   });
   //открытие страницы во вкладке
   await fn.openTab(tab, url).then(res => {
-    res.err ? (err = res.err) : (tab = res.data);
+    res.err ? (console.log(res.err)) : (tab = res.data);
   });
 
   //цикл для обхода всех страниц
   while (true) {
-    await tab.wait(2000)
+    await tab.wait(3000)
     //переменная для зранения текста ошибки
     let err = null;
+    //переменная ля хранения html кода
     let html;
     if (!err) {
       //получение BODY у страницы
@@ -51,23 +52,29 @@ module.exports.start = async () => {
       if (ind == 0) {
         //получение найденных значений
         lengthPages = await fn.getLengthListPage(html);
-        lengthPages = Math.ceil(lengthPages / countOnPage);
-        console.log("TCL: module.exports.start -> lengthPages", lengthPages)
+        console.log("Количество товаров для парсинга", lengthPages)
+        //округление до большего числа для получения страниц
+        lengPage = Math.ceil(lengthPages / countOnPage);
+        console.log("Количестов страниц для парсинга", lengPage)
       }
       try {
+        // нажатие на кнопку дял получения новго списка продуктов
         await tab.click(`.k-i-arrow-e`)
       } catch (err) {
-        console.log("An error occured:", err)
+        console.log("Произошла ошибка:", err)
       }
-      fn.getListProduct(html)
+      //получение списка продуктов
+      await fn.getListProduct(html)
       ind++
-      if (ind == lengthPages+1) {
-        console.log("Длина", arr.length)
-        fn.saveToFile()
+      if (ind == lengPage) {
+        await fn.saveToFile()
         break
       }
-      //fn.getListProduct(html)
+    
     }
+   
   }
 };
 module.exports.arr = arr;
+
+module.exports.lengthPages = lengthPages
